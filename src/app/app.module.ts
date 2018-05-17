942473f7a0940da7a3ae5088d6f02906e725e3a6
TODO1\mcepeda <mcepeda@todo1.com>
2018-05-17 16:38:55 -0500

    Primera version tienda online Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HomeProductsComponent } from './components/product/home-products/home-products.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { StockComponent } from './components/product/stock/stock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    HomeProductsComponent,
+    FooterComponent,
+    StockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
